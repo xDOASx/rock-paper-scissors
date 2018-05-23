11e17e13cf233dcd2797c42fb50760b938236604
Yong Bakos <ybakos@humanoriented.com>
2018-05-23 07:36:08 -0700

    Test game starting.

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,13 @@
 class Game
 
+  attr_reader :started
+
   def start
+    @started = true
+  end
+
+  def started?
+    self.started || false
   end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -10,4 +10,15 @@ describe 'A game of rock paper scissors' do
     Game.new.start
   end
 
+  it 'is not initially started' do
+    game = Game.new
+    expect(game).to_not be_started
+  end
+
+  it 'a started game knows that it has started' do
+    game = Game.new
+    game.start
+    expect(game).to be_started
+  end
+
 end
