95678659c47c0edfc541c7f68d012e44b64efe94
xDOASx <benesbit@yahoo.com>
2019-06-05 11:42:43 -0700

    Shameless green

--- lib/game.rb
+++ lib/game.rb
@@ -1,22 +1,34 @@
 class Game
 
-  attr_reader :started
+  attr_reader :started, :finished
 
   def start
     @started = true
+    @finished = false
   end
 
   def started?
     self.started || false
   end
 
+  def finished?
+    self.finished || false
+  end
+
   def play(left = nil, right = nil)
     raise 'Game must first be started' unless started?
     return nil unless (left && right)
-    if (left == :rock && right == :scissors)
+    if ((left == :rock && right == :scissors) | (left == :scissors && right == :rock))
+      @finished = true;
       "Rock beats scissors!"
+    elsif ((left == :rock && right == :paper) | (left == :paper && right == :rock))
+      @finished = true;
+      "Paper beats rock!"
+    elsif ((left == :scissors && right == :paper) | (left == :paper && right == :scissors))
+      @finished = true;
+      "Scissors beats paper!"
     else
-      :UNKNOWN
+      "Tie game. Try again!"
     end
   end
 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -57,29 +57,53 @@ describe 'A game of rock paper scissors' do
     end
 
     context 'rock vs scissors' do
+      game = Game.new
+      game.start
       it 'announces the correct winner' do
-        game = Game.new
-        game.start
         expect(game.play(:rock, :scissors)).to eq("Rock beats scissors!")
         expect(game.play(:scissors, :rock)).to eq("Rock beats scissors!")
       end
-      skip('is finished')
+      it 'is finished' do
+        expect(game.finished).to be_truthy
+      end
     end
 
     context 'rock vs paper' do
-      skip('returns "Paper beats rock!"')
-      skip('is finished')
+      game = Game.new
+      game.start
+      it 'announces the correct winner' do
+        expect(game.play(:rock, :paper)).to eq("Paper beats rock!")
+        expect(game.play(:paper, :rock)).to eq("Paper beats rock!")
+      end
+      it 'is finished' do
+        expect(game.finished).to be_truthy
+      end
     end
 
 
     context 'scissors vs paper' do
-      skip('returns "Scissors vs paper!"')
-      skip('is finished')
+      game = Game.new
+      game.start
+      it 'announces the correct winner' do
+        expect(game.play(:scissors, :paper)).to eq("Scissors beats paper!")
+        expect(game.play(:paper, :scissors)).to eq("Scissors beats paper!")
+      end
+      it 'is finished' do
+        expect(game.finished).to be_truthy
+      end
     end
 
     context 'a tie game' do
-      skip('returns "Tie game. Try again!"')
-      skip('is not finished')
+      game = Game.new
+      game.start
+      it 'announces a correct tie game' do
+        expect(game.play(:rock, :rock)).to eq("Tie game. Try again!")
+        expect(game.play(:paper, :paper)).to eq("Tie game. Try again!")
+        expect(game.play(:scissors, :scissors)).to eq("Tie game. Try again!")
+      end
+      it 'is not finished' do
+        expect(game.finished).to be_falsey
+      end
     end
 
   end
